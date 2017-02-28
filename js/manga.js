1a10c81911b2d7201521107e0ad2857bdea6d296
Mackenzie <ma.luzarow@gmail.com>
2017-02-28 11:25:03 -0600

    Added a method for scrolling through manga
    Added scrolling buttons that iterate through images in the mangaColors
    folder.

--- js/manga.js
+++ js/manga.js
@@ -1,16 +1,42 @@
 /* On element load, size page correctly and show first image */
-var manga_box = null;
-var nav_bar = null;
+var manga_img = null;
+var page_images = new Array ();
+var index = 0;
 
 window.onload = function () {
 	initialize ();
 }
 
 function initialize () {
-	manga_box = document.getElementById ("manga_box");
-	nav_bar = document.getElementById ("navbar");
+	manga_img = document.getElementById ("manga_img");
 	
-	// Set the size of the manga view window
-	manga_box Height = Viewport height - nav_bar height;
-	
+	page_images.push ("../img/art/mangaColors/c005colorp007.png");
+    page_images.push ("../img/art/mangaColors/c005colorp008.png");
+    page_images.push ("../img/art/mangaColors/c005colorp009.png");
+    page_images.push ("../img/art/mangaColors/c005colorp010.png");
+    page_images.push ("../img/art/mangaColors/c005colorp011.png");
+    page_images.push ("../img/art/mangaColors/c005colorp012.png");
+    page_images.push ("../img/art/mangaColors/c005colorp013.png");
+    page_images.push ("../img/art/mangaColors/c005colorp014.png");
+    page_images.push ("../img/art/mangaColors/c005colorp015.png");
+}
+
+function left () {
+    index -= 1;
+    
+    if (index < 0) {
+        index = 0;
+    } else {
+        manga_img.src = page_images[index];
+    }
+}
+
+function right () {
+    index += 1;
+    
+    if (index >= page_images.length) {
+        index = page_images.length - 1;
+    } else {
+        manga_img.src = page_images[index];
+    }   
 }
