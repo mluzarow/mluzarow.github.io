8398abc3b851e8edaa298e94d46c2086b3ea3c05
Mackenzie <ma.luzarow@gmail.com>
2017-03-28 22:06:26 -0500

    Redesigned appboxes + changed animation
    Redesigned the app boxes so that the link buttons would no longer float
    off the div during resizes. Changed animation style,

--- js/appBoxes.js
+++ js/appBoxes.js
@@ -3,6 +3,9 @@ var boxes;
 /* Animation vars */
 var BOX_OPEN_WIDTH = "47vw";
 var INFO_OPEN_WIDTH = "25vw";
+var BOX_CLOSED_WIDTH = "22vw";
+var INFO_CLOSED_WIDTH = "0vw";
+var su = 0;
 
 window.onload = function () {
     // Find all boxes on page
@@ -36,50 +39,63 @@ function animate (target) {
     } else {
         // Check id to get open / closed status
         if (this.id == "closed") {
-            // Inner text fade-in must be delayed until container is open
-            this.getElementsByClassName ("project_comments")[0].style.transitionDelay = "2s";
+            // WARNING: transition property MUST go before transitionDelay or it will
+            // NOT FIRE!!!
+        
+            // Transition Properties
+            this.style.transition = "width 1s";
+            this.getElementsByClassName ("project_info")[0].style.transition = "width 0s, opacity 0.7s";
+            
+            // Transition Delays
             this.style.transitionDelay = "0s";
-            this.style.transition = "width 2s";
-            this.getElementsByClassName ("project_info")[0].style.transitionDelay = "0s";
-            this.getElementsByClassName ("project_info")[0].style.transition = "width 2s";
-            
-            this.style.width = BOX_OPEN_WIDTH;
-            this.getElementsByClassName ("project_comments")[0].style.visibility = "visible";
-            this.getElementsByClassName ("project_comments")[0].style.opacity = 1;
-            this.getElementsByClassName ("project_info")[0].style.width = INFO_OPEN_WIDTH;
-            this.getElementsByClassName ("project_extender")[0].innerHTML = "</br></br></br></br><";
-            
-            // Time a function to disable transitions after they have transitioned.
-            // This is to keep from animating width on resizeBy
-            setTimeout (turnOffTransitions, 2000);
-            
-            this.id = "open";
-        } else {
-            // Container must be delayed until text has faded out
-            this.getElementsByClassName ("project_comments")[0].style.transitionDelay = "0s";
-            this.style.transitionDelay = "1s";
             this.getElementsByClassName ("project_info")[0].style.transitionDelay = "1s";
             
-            this.style.transition = "width 2s";
-            this.getElementsByClassName ("project_info")[0].style.transition = "width 2s";
+            // Misc Variables
+            this.getElementsByClassName ("project_extender")[0].innerHTML = "</br></br></br></br><";
+            this.id = "open";
             
-            this.style.width = "22vw";
-            this.getElementsByClassName ("project_comments")[0].style.visibility = "hidden";
-            this.getElementsByClassName ("project_comments")[0].style.opacity = 0;
-            this.getElementsByClassName ("project_info")[0].style.width = "0vw";
-            this.getElementsByClassName ("project_extender")[0].innerHTML = "</br></br></br></br>>";
+            // Transition Variables
+            this.style.width = BOX_OPEN_WIDTH;
+            this.getElementsByClassName ("project_info")[0].style.width = INFO_OPEN_WIDTH;
+            this.getElementsByClassName ("project_info")[0].style.opacity = 1;
             
             // Time a function to disable transitions after they have transitioned.
             // This is to keep from animating width on resizeBy
-            setTimeout (turnOffTransitions, 2000);
+            setTimeout (turnOffTransitions.bind (this), 2000);
+        } else {
+            // Transition Properties
+            this.style.transition = "width 1s";
+            this.getElementsByClassName ("project_info")[0].style.transition = "width 0s, opacity 0s";
+            
+            // Transition Delays
+            this.style.transitionDelay = "0.1s";
+            this.getElementsByClassName ("project_info")[0].style.transitionDelay = "0s";
+            
+            // Misc Variables
+            this.getElementsByClassName ("project_extender")[0].innerHTML = "</br></br></br></br>>";
             this.id = "closed";
+            
+            // Transition Variables
+            this.style.width = BOX_CLOSED_WIDTH;
+            this.getElementsByClassName ("project_info")[0].style.width = INFO_CLOSED_WIDTH;
+            this.getElementsByClassName ("project_info")[0].style.opacity = 0;
+            
+            // Time a function to disable transitions after they have transitioned.
+            // This is to keep from animating width on resizeBy
+            setTimeout (turnOffTransitions.bind (this), 3000);
         }
     }
 }
 
 function turnOffTransitions () {
-    for (var i = 0; i < boxes.length; i++) {
-        boxes[i].style.transition = "none";
-        boxes[i].getElementsByClassName ("project_info")[0].style.transition = "none";
+    if (su == 0) {
+        for (var i = 0; i < boxes.length; i++) {
+            boxes[i].style.transition = "none";
+            boxes[i].getElementsByClassName ("project_info")[0].style.transition = "none";
+        }
+        su = 1;
+    } else {
+        this.style.transition = "none";
+        this.getElementsByClassName ("project_info")[0].style.transition = "none";
     }
 }
