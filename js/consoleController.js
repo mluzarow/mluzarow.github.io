90447d159663f21b70ca3c885b608b54eec069ba
Mackenzie <ma.luzarow@gmail.com>
2017-11-18 14:23:45 -0600

    Something totally different again.

---
+++ js/consoleController.js
@@ -0,0 +1,9 @@
+var input;
+var output;
+
+window.onload = function () {
+    input = document.getElementById ("text-input-box");
+    output = document.getElementById ("text-output");
+
+    input.value = "";
+}
