b9f9aadb2e0b7b3b7453f715a7432f35d40b0f80
mluzarow <ma.luzarow@gmail.com>
2018-12-29 14:55:40 -0600

    Updated SiteBuilder to be not static.

--- js/site_builder.js
+++ js/site_builder.js
@@ -1,87 +1,101 @@
+// Load the template base path
+var basePath = document.currentScript.src.substr (
+	0,
+	document.currentScript.src.length - 18
+) + "templates";
+
 /**
  * Builds out the page replacing predefined IDs with the corresponding template.
  */
 class SiteBuilder {
-    /**
-     * Runs the SiteBuilder routine.
-     */
-    static initialize () {
-        // Get every <br> tag
-        var brs = document.getElementsByTagName ("br");
-    
-        // Check tag IDs for valid template IDs
-        for (var i = 0; i < brs.length; i++) {
-            var start = brs[i].id.indexOf ("template-");
-    
-            if (start > -1) {
-                this.requestTemplate (brs[i].id.substr (start + 9, brs[i].id.length - 8), brs[i].id);
-            }
-        }
-    }
-    
-    /**
-     * Gets the text content of a requested local file by file name.
-     *
-     * @param {string} filename The name of the template to be used.
-     * @param {string} tag      The ID tag of the script to be replaced with template
-     *                          data.
-     */
-    static requestTemplate (fileName, tag) {
-        // Create new request
-        var resp = new XMLHttpRequest ();
-        resp.tag = tag;
-        
-        // Check the hostname; if there is none, page is in debug mode.
-        // if (window.location.hostname === "") {
-        //     console.log ("Sitebuilder in debug mode targetting [" + fileName + "].");
-        //     var fileLocation = "file:///C:/Users/Mark/Desktop/Github Repo Clones/mluzarow.github.io/templates/" + fileName + ".html";
-        // } else {
-            var fileLocation = "https://mluzarow.github.io/templates/" + fileName + ".html";
-        // }
-        
-        // Event trigger on response answer received or timeout
-        resp.onreadystatechange = function() {
-            // Answer received
-            if (this.readyState == 4) {
-                // Success
-                if (this.status == 200) {
-                    // Send response text to helper
-                    SiteBuilder.renderTemplate (this.responseText, this.tag);
-                // Fail
-                } else {
-                    // Send null (failure) to helper
-                    SiteBuilder.renderTemplate (null, this.tag);
-                }
-            }
-        };
-        
-        // Send request
-        resp.open ("GET", fileLocation);
-        resp.overrideMimeType("text/plain")
-        resp.send ();
-    }
-    
-    /**
-     * Replaces received template data with the element using the tag ID.
-     *
-     * @param  {string} content Text content of the HTML template.
-     * @param  {string} tag     The ID tag of the script to be replaced with template
-     *                          data.
-     */
-    static renderTemplate (content, tag) {
-        var target = document.getElementById (tag);
-        
-        // Check for null input
-        if (content === null) {
-            // Failure
-            var errorElement = document.createElement ("span");
-            errorElement.style.color = "red";
-            errorElement.innerHTML = "ERROR: Could not render requested template.";
-            target.outerHTML = errorElement.outerHTML;
-            
-            return;
-        }
-        
-        target.outerHTML = content;
-    }
+	/**
+	 * Constructor for site builder.
+	 */
+	constructor () {
+		/**
+		 * @var {String}  basePath  template folder base path
+		 * @var {Boolean} flagDebug debug flag if there is no hostname (running
+		 *                          locally)
+		 */
+		this.basePath = basePath;
+		this.flagDebug = window.location.hostname.length === 0;
+		
+		if (this.flagDebug === true) {
+			console.debug ("SiteBuilder running in debug mode.");
+		}
+	}
+	
+	/**
+	 * Builds all the page's templates.
+	 */
+	buildTemplates () {
+		if (this.flagDebug === true) {
+			console.debug (`Using basepath ${this.basePath}.`);
+		}
+		
+		// Get every <br> tag
+		let placeholders = document.getElementsByClassName ("sb-template");
+		
+		for (let i = 0; i < placeholders.length; i++) {
+			this._requestTemplate (
+				placeholders[i].dataset.template,
+				placeholders[i]
+			);
+		}
+	}
+	
+	/**
+	 * Gets the text content of a requested local file by file name.
+	 * 
+	 * @param {String}  filename     name of the template to be used
+	 * @param {Element} $placeholder placeholder element
+	 */
+	_requestTemplate (fileName, $placeholder) {
+		let request = new XMLHttpRequest ();
+		request.tag = $placeholder;
+		
+		if (this.flagDebug === true) {
+			// No hostname, so running locally in debug mode
+			console.debug (`Requesting template [${fileName}].`);
+		}
+		
+		// Event trigger on response answer received or timeout
+		request.onreadystatechange = function() {
+			if (request.readyState == 4) {
+				// Answer received
+				
+				if (request.status != 200) {
+					// Failure
+					return this._renderTemplate ("", request.tag);
+				}
+				
+				return this._renderTemplate (request.responseText, request.tag);
+			}
+		}.bind (this);
+		
+		// Send request
+		request.open ("GET", `${this.basePath}/${fileName}.html`);
+		request.overrideMimeType ("text/plain");
+		request.send ();
+	}
+	
+	/**
+	 * Replaces received template data with the element using the tag ID.
+	 *
+	 * @param {String}  content      template HTML
+	 * @param {Element} $placeholder placeholder element
+	 */
+	_renderTemplate (content, $placeholder) {
+		if (content.length === 0) {
+			// No HTML to display
+			var errorElement = document.createElement ("span");
+			errorElement.style.color = "red";
+			errorElement.innerHTML = "ERROR: Could not render requested template.";
+			$placeholder.outerHTML = errorElement.outerHTML;
+			
+			return;
+		}
+		
+		$placeholder.outerHTML = content;
+	}
 }
