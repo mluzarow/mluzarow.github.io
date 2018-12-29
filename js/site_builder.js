4aeb13cfc986bf6c0fb998d3994159e58f3ecae9
mluzarow <ma.luzarow@gmail.com>
2018-12-29 16:23:34 -0600

    Added class for app_display customization.

--- js/site_builder.js
+++ js/site_builder.js
@@ -2,7 +2,7 @@
 var basePath = document.currentScript.src.substr (
 	0,
 	document.currentScript.src.length - 18
-) + "templates";
+);
 
 /**
  * Builds out the page replacing predefined IDs with the corresponding template.
@@ -35,13 +35,32 @@ class SiteBuilder {
 		
 		// Get every <br> tag
 		let placeholders = document.getElementsByClassName ("sb-template");
-		
 		for (let i = 0; i < placeholders.length; i++) {
 			this._requestTemplate (
 				placeholders[i].dataset.template,
 				placeholders[i]
 			);
 		}
+	}
+	
+	/**
+	 * Sets a config for data to be used with certain templates.
+	 * 
+	 * @param {Object} config template data config
+	 */
+	setConfig (config) {
+		this.config = config;
+	}
+	
+	_customizeTemplate (content, data, templateName) {
+		let builder;
+		
+		switch (templateName) {
+			case "app_display": builder = new AppDisplayBuilder (); break;
+			default:            return content;
+		}
+		
+		return builder.customize (content, data);
 	}
 	
 	/**
@@ -74,7 +93,7 @@ class SiteBuilder {
 		}.bind (this);
 		
 		// Send request
-		request.open ("GET", `${this.basePath}/${fileName}.html`);
+		request.open ("GET", `${this.basePath}templates/${fileName}.html`);
 		request.overrideMimeType ("text/plain");
 		request.send ();
 	}
@@ -96,6 +115,174 @@ class SiteBuilder {
 			return;
 		}
 		
+		if (
+			$placeholder.dataset.tdata &&
+			this.config[$placeholder.dataset.tdata]
+		) {
+			content = this._customizeTemplate (
+				content,
+				this.config[$placeholder.dataset.tdata],
+				$placeholder.dataset.template
+			);
+		}
+		
 		$placeholder.outerHTML = content;
+	}
+}
+
+/**
+ * Builder for customizing the app_display template.
+ */
+class AppDisplayBuilder {
+	/**
+	 * Customizes the app_display template with the given config data.
+	 * 
+	 * @param {String} content base template HTML
+	 * @param {Object} data    dictionary of config options
+	 * 
+	 * @return {String} customized HTML
+	 */
+	customize (content, data) {
+		content = this._processDependencies (content, data);
+		content = this._processImageSource (content, data);
+		content = this._processLinkGithub (content, data);
+		content = this._processLinkLive (content, data);
+		content = this._processSummary (content, data);
+		content = this._processTitle (content, data);
+		
+		return content;
+	}
+	
+	/**
+	 * Processes the dependencies config option.
+	 * 
+	 * @param {String} content base template HTML
+	 * @param {Object} data    dictionary of config options
+	 * 
+	 * @return {String} customized HTML
+	 */
+	_processDependencies (content, data) {
+		if (
+			data.hasOwnProperty ("dependencies") === false ||
+			Array.isArray (data.dependencies) === false ||
+			data.dependencies.length === 0
+		) {
+			return content.replace (/{@dependencies}/g, "<span>None!</span>");
+		}
+		
+		let deps = "";
+		
+		for (let i = 0; i < data.dependencies.length; i++) {
+			let item = "";
+			
+			if (data.dependencies[i].hasOwnProperty ("name") === true) {
+				item = data.dependencies[i].name;
+			} else {
+				item = "Unnamed Dependency";
+			}
+			
+			if (data.dependencies[i].hasOwnProperty ("name") === true) {
+				item = `<a href="${data.dependencies[i].name}">${item}</a>`;
+			}
+			
+			deps += `<span>${item}</span>`;
+		}
+		
+		return content.replace (/{@dependencies}/g, deps);
+	}
+	
+	/**
+	 * Processes the image source config option.
+	 * 
+	 * @param {String} content base template HTML
+	 * @param {Object} data    dictionary of config options
+	 * 
+	 * @return {String} customized HTML
+	 */
+	_processImageSource (content, data) {
+		if (data.hasOwnProperty ("imgSrc") === false) {
+			return content.replace ("{@imgSrc}", `${basePath}img/noimage.png`);
+		}
+		
+		return content.replace ("{@imgSrc}", data.imgSrc);
+	}
+	
+	/**
+	 * Processes the github link config option.
+	 * 
+	 * @param {String} content base template HTML
+	 * @param {Object} data    dictionary of config options
+	 * 
+	 * @return {String} customized HTML
+	 */
+	_processLinkGithub (content, data) {
+		if (data.hasOwnProperty ("linkGh") === false) {
+			return content.replace ("{@linkGh}", "https://github.com/mluzarow");
+		}
+		
+		return content.replace ("{@linkGh}", data.linkGh);
+	}
+	
+	/**
+	 * Processes the live link config option.
+	 * 
+	 * @param {String} content base template HTML
+	 * @param {Object} data    dictionary of config options
+	 * 
+	 * @return {String} customized HTML
+	 */
+	_processLinkLive (content, data) {
+		if (data.hasOwnProperty ("linkLive") === false) {
+			return content.replace ("{@linkLive}", "https://mluzarow.github.io/");
+		}
+		
+		return content.replace ("{@linkLive}", data.linkLive);
+	}
+	
+	/**
+	 * Processes the summary config option.
+	 * 
+	 * @param {String} content base template HTML
+	 * @param {Object} data    dictionary of config options
+	 * 
+	 * @return {String} customized HTML
+	 */
+	_processSummary (content, data) {
+		let pTab = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+		
+		if (
+			data.hasOwnProperty ("summary") === false ||
+			Array.isArray (data.summary) === false ||
+			data.summary.length === 0
+		) {
+			return content.replace (
+				"{@summary}",
+				`<p>${pTab}<i>No summary.</i></p>`
+			);
+		}
+		
+		let summary = "";
+		
+		for (let i = 0; i < data.summary.length; i++) {
+			summary += `<p>${pTab}${data.summary[i]}</p>`;
+		}
+		
+		return content.replace ("{@summary}", summary);
+	}
+	
+	/**
+	 * Processes the title config option.
+	 * 
+	 * @param {String} content base template HTML
+	 * @param {Object} data    dictionary of config options
+	 * 
+	 * @return {String} customized HTML
+	 */
+	_processTitle (content, data) {
+		if (data.hasOwnProperty ("title") === false) {
+			return content.replace ("{@title}", "Untitled Game");
+		}
+		
+		return content.replace ("{@title}", data.title);
 	}
 }
